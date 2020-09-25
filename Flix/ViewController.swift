d16ca2bdffd4b81ee6622798f7af1693d32b2e6b
Saad Mahboob <smahboob@fandm.edu>
2020-09-24 23:06:05 -0400

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Saad Mahboob on 24/09/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
